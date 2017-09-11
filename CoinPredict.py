bd15c8a40dfc472934eda4208359cf8bcf0e98e0
fermi_dirac <ManueleBryan@gmail.com>
2017-09-11 00:29:52 -0700

    Database now works

--- CoinPredict.py
+++ CoinPredict.py
@@ -1,15 +1,17 @@
-"The main function for this program. Run this to update your config, update your model, and begin predicting in real time"
-
-import time
-import gdax
+"""The main function for this program. Run this to update your config,
+update your model, and begin predicting in real time"""
 
 from data_management import config
+from data_management import database
 
 if __name__ == '__main__':
-    conf = config.Config()
+    CONF = config.Config()
+    DB = database.Database()
 
     print 'would you like to setup db config? \'yes\'/\'no\''
     config_db = raw_input()
 
     if config_db == 'yes':
-        conf.setup_db_config()
+        CONF.setup_db_config()
+
+    DB.initialize_db()
--- data_management/Database.py
+++ data_management/Database.py
@@ -6,9 +6,17 @@ from data_management import config
 class Database(object):
     """Interact with the database for CoinPredict"""
 
+    TABLE_NAMES = {
+        'historic_raw_data': 'historic_raw_data'
+    }
+
+    HISTORIC_SCHEMA = 'time bigint PRIMARY KEY, low money, high money, open money, close money, volume double'
+
     def __init__(self):
         self.conf = config.Config()
         self.connect()
+
+        self.initialize_db()
 
     def connect(self):
         """creates a connection and a cursor to the PostgreSQL database server"""
@@ -24,13 +32,20 @@ class Database(object):
             print error
 
     def initialize_db(self):
-        """initializes db by checking if one exists and creating or
-            updating schemas if it doesn't"""
-        pass
+        """initializes db"""
+        self.create_historic_raw_data()
+
 
     def create_historic_raw_data(self):
         """creates historic raw data table if it's empty"""
-        pass
+        self.cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables \
+            WHERE table_name='%s';", (self.TABLE_NAMES['historic_raw_data']))
+        is_historic_table_initalized = bool(self.cur.rowcount)
+
+        if not is_historic_table_initalized:
+            self.cur.execute("CREATE TABLE %s (%s);", \
+                (self.TABLE_NAMES['historic_raw_data'], self.HISTORIC_SCHEMA))
+
 
     def close_connection(self):
         """closes db connection"""
